20964c27c1711b6c4824c58b76428945e6efcba9
lourenco010007 <lourencoderogerios@gmail.com>
2026-06-09 11:12:39 +0200

    Configuracoes iniciais

---
+++ eventos/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('title', 'Pagina Inicial')
+
+@section('content')
+
+<div class="container">
+    <h1>Bem-vindo ao Eventos!</h1>
+    <p>Encontre os melhores eventos da cidade e participe de momentos inesquecíveis.</p>
+
+@endsection
